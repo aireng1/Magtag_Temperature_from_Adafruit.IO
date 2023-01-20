ec56efe0e333dc9b5d23e47f079f3469e2fe5a2f
aireng1 <57272712+aireng1@users.noreply.github.com>
2023-01-20 13:38:40 +0000

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+secrets = {
+    'ssid': 'wifi name here',
+    'password': 'wifi password here',
+    'aio_username': 'username here',
+    'aio_key': 'key here',
+    'aio_feed': "username/feed here",
+    'port': 1883,
+    'timezone': 'your timezone',
+    }
